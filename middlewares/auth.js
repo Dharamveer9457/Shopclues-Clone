279554d529203c10f9777ef9990d63951cdcc810
Dharamveer <dharammveer945734@outlook.com>
2023-05-31 04:26:49 +0530

    final commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@
 const jwt = require("jsonwebtoken")
 
 function auth(req,res,next){
-    const token12 = req.headers.authorization
+    const token1 = req.headers.authorization
     if(token){
         try {
          const decoded = jwt.verify(token.split(" ")[1],"blog")
