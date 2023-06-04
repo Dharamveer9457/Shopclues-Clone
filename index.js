5d6f9806b5a31738cb515fe1a33618345a2ffd18
Dharamveer <dharammveer945734@outlook.com>
2023-06-04 13:03:38 +0530

    Finally done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {auth} = require("./middlewares/auth")
 const cors = require("cors")
 require('dotenv').config()
 
-const lyu= 123456;
+const lyu= 12345
 app.use(express.json())
 app.use(cors())
 
