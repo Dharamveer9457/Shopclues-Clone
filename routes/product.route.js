0287e585226311de24c2c81520a4055895b7dd33
Dharamveer <dharammveer945734@outlook.com>
2023-06-09 22:01:18 +0530

    Final Commit

--- routes/product.route.js
+++ routes/product.route.js
@@ -2,6 +2,7 @@ const express = require("express")
 const {productModel} = require("../models/product.model")
 const productRouter = express.Router()
 
+let abc = 12345
 
 //get routes for all the articles
 productRouter.get("/", async(req,res)=>{
